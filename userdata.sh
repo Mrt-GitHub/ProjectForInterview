70754e0bd3736d5878bfb095426e1f3b164aa3a0
Mrt-GitHub <bern.murat@gmail.com>
2022-05-16 14:50:43 +0200

    setting files has been added

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,12 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+git clone https://github.com/Mrt-GitHub/ProjectForInterview.git
+cd /home/ubuntu/ProjectForInterview
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/ProjectForInterview/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
